bd7e4f83239c51cd660b4630ab8861a3e38fc65e
RACHID EL HACHIMI <rachidelhachimi6@gmail.com>
2023-11-21 00:29:34 +0100

    feat: counting occurences using split function.

--- Basics/3_count-occurences/count-occurences.js
+++ Basics/3_count-occurences/count-occurences.js
@@ -1,12 +1,17 @@
-function countOccurences(str, character) {
-    let count = 0;
+// function countOccurences(str, character) {
+//     let count = 0;
 
-    for (let i = 0; i < str.length; i++)
-    {
-        if (str[i] === character)
-            count++;
-    }
-    return (count);
-}
+//     for (let i = 0; i < str.length; i++)
+//     {
+//         if (str[i] === character)
+//             count++;
+//     }
+//     return (count);
+// }
+
+
+// another way 
+
+const countOccurences = (str, char) => str.split(char).length - 1;
 
 module.exports = countOccurences;
