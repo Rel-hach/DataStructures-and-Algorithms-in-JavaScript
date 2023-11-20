85f4d5019f83d0d2c70fb98fe940f1398082af5d
RACHID EL HACHIMI <rachidelhachimi6@gmail.com>
2023-11-21 00:29:50 +0100

    tested.

--- Basics/3_count-occurences/count-occurences-run.js
+++ Basics/3_count-occurences/count-occurences-run.js
@@ -2,4 +2,8 @@ const countOccurences = require('./count-occurences');
 
 const x = countOccurences('this is a test', 't');
 
+const y = countOccurences('hello world', 'l');
+
 console.log(x);
+
+console.log(y);
