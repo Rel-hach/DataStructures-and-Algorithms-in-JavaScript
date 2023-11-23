03de1ca9a098e76848d2c4c304041e99ea09e9a9
RACHID EL HACHIMI <rachidelhachimi6@gmail.com>
2023-11-23 21:47:56 +0100

    feat: Add findMaxNumber function.

---
+++ Basics/4_find-max-number/find-max-number-run.js
@@ -0,0 +1,9 @@
+const findMaxNumber = require('./find-max-number');
+
+let maxNum = findMaxNumber([10, 25 , 63, 50, 10]);
+
+console.log(maxNum);
+
+maxNum = findMaxNumber([-10, -25 , -63, -50, -10]);
+
+console.log(maxNum);
