0687fa0256767b5bbab995ec06d10b19cc5f6837
RACHID EL HACHIMI <rachidelhachimi6@gmail.com>
2023-11-23 22:58:42 +0100

    feat: add reverseString func to reverse strings.

---
+++ Basics/6_reverse-string/reverse-string-run.js
@@ -0,0 +1,5 @@
+const reverseString = require ('./reverse-string');
+
+const rev = reverseString("! gniod uoy era woh ,enoyreve olleH");
+
+console.log(rev);
---
+++ Basics/6_reverse-string/reverse-string.js
@@ -0,0 +1,32 @@
+function reverseString(str) {
+
+    let reversedStr = "";
+    let x = str.length - 1;
+
+    for (let i = 0; i < str.length; i++){
+        reversedStr += str[x - i];
+    }
+    return (reversedStr);
+}
+
+
+/*
+    function reverseString(str) {
+
+    let reversedStr = "";
+    let x = str.length - 1;
+
+    for (let i = x; i >= 0; i--){
+        reversedStr += str[i];
+    }
+    return (reversedStr);
+}
+*/
+
+
+
+// const reverseString = (str) => str.split("").reverse().join('');
+
+// in the last example we split the string to have an array so we can reverse it with reverse () then we join it again.
+
+module.exports = reverseString;
