44c451e54ea93e5f45d83ac275597761ec8570ba
RACHID EL HACHIMI <rachidelhachimi6@gmail.com>
2023-12-12 21:37:15 +0100

    feat: run & test the find-missing-num

---
+++ Basics/15_find-missing-number.js/find-missing-number-run.js
@@ -0,0 +1,8 @@
+const findMissingNumber = require("./find-missing-number");
+
+console.log(findMissingNumber([1,2,3,5,6,7,8,9]));
+
+console.log(findMissingNumber([1,2,3,4,5,6,8,9]));
+
+
+console.log(findMissingNumber([9,4,3,6,7,8,1,5]));
