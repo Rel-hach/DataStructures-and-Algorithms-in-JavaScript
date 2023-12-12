15d4d1edc0287946e607c4711bf2c2ada8792de0
RACHID EL HACHIMI <rachidelhachimi6@gmail.com>
2023-12-12 21:04:07 +0100

    feat: run & test

---
+++ Basics/14_display-likes/display-likes-run.js
@@ -0,0 +1,17 @@
+
+const displayLikes = require('./display-likes');
+
+displayLikes(['tariq']);
+
+console.log('\n');
+
+displayLikes(['mohamed', 'khalid']);
+
+console.log('\n');
+
+displayLikes(['mohamed', 'khalid', 'hicham']);
+
+console.log('\n');
+
+displayLikes(['alex', 'khalid', 'ahmed', 'moad']);
+
