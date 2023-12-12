be3af7ae787f93b8d16573a95ffcb8ed02cfbdcb
RACHID EL HACHIMI <rachidelhachimi6@gmail.com>
2023-12-13 00:12:33 +0100

    feat: test and run charsAreUnque func

---
+++ Basics/17_chars-are-unique/chars-are-unique-run.js
@@ -0,0 +1,9 @@
+const charsAreUnique = require("./chars-are-unique");
+
+console.log(charsAreUnique("abcdef"));
+
+console.log(charsAreUnique('efgklmnf'));
+
+
+console.log(charsAreUnique(['a', 'b', 'c', 'd', 'e', 'a']));
+
