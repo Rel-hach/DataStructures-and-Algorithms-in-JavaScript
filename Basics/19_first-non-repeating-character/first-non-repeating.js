ed62199523d585ff178b553a3e1cc2b58b5480fe
RACHID EL HACHIMI <rachidelhachimi6@gmail.com>
2023-12-13 14:35:39 +0100

    refactor: return null when the string is empty.

--- Basics/19_first-non-repeating-character/first-non-repeating.js
+++ Basics/19_first-non-repeating-character/first-non-repeating.js
@@ -3,15 +3,14 @@ const firstNonRepeating = (str) => {
 
     const  counter= {};
 
-    for (const c of str)
-    {
-        counter[c] = ( counter[c] || 0) + 1;
-    }
+    for (const c of str){
+        counter[c] = ( counter[c] || 0) + 1; }
 
     for (const element in counter) {
         if (counter[element] == 1)
-            return (element);
-    }
+            return (element); }
+
+    return (null);
 }
 
 module.exports = firstNonRepeating;
